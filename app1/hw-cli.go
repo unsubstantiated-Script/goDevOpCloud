b7334f53a1df191592b077c65c1cbc89c963f832
unsubstantiated-Script <jusfulton@gmail.com>
2024-01-27 10:28:05 -0800

    conditional CLI

--- app1/hw-cli.go
+++ app1/hw-cli.go
@@ -7,5 +7,11 @@ import (
 
 func RollHWCLI() {
 	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Printf("Useage: ./hw-cli <argument>\n")
+		os.Exit(1)
+	}
+
 	fmt.Printf("Dis is de args\nos.Args: %v\n \nArgument: %v\n", args, args[1:len(args)])
 }
