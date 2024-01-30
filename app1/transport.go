5ede52a953f4c1d22da48c2070433461efa14d60
unsubstantiated-Script <jusfulton@gmail.com>
2024-01-29 21:03:01 -0800

    JWT jazz done

---
+++ app1/transport.go
@@ -0,0 +1,16 @@
+package app1
+
+import "net/http"
+
+type MyJWTTransport struct {
+	transport http.RoundTripper
+	token     string
+}
+
+func (m MyJWTTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	if m.token != "" {
+		req.Header.Add("Authorization", "Bearer "+m.token)
+	}
+
+	return m.transport.RoundTrip(req)
+}
